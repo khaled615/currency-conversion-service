eabb573352b9cfcbd043481204d3e5a848a692be
khaled615 <khaled.benhafsia07@gmail.com>
2021-01-06 16:11:04 +0100

    Version

--- src/main/java/com/in45minutes/microservices/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/in45minutes/microservices/CurrencyExchangeServiceProxy.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.netflix.ribbon.proxy.annotation.ClientProperties;
+
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
 //A cause du Ribbon, nous configurions l'URL d'une autre manniére.
 @FeignClient(name="currency-exchange-service")
@@ -13,7 +15,7 @@ public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}") 
 	public CurrencyConversionBean retrieveExchangeValue
-	(@PathVariable("from") String from, @PathVariable("to") String to);
+	(@PathVariable("from") String from, @PathVariable("to") String to); 
 		
 	
 }
